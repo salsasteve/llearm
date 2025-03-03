82aa04d7009d569398dd34e847dbaa19792c15e4
Salsa Steve <elmanstevelaguna@gmail.com>
2025-03-03 06:58:48 +0000

    Add rgb_blinky

---
+++ esp-hal_v0.23.1/rgb_blinky/src/bin/async_main.rs
@@ -0,0 +1,132 @@
+//! This example shows how to use the interrupt executors to prioritize some
+//! tasks over others.
+//!
+//! The example creates three tasks:
+//!  - A low priority task that is not actually async, but simulates some
+//!    blocking work. This task will run for 5 seconds, then sleep for 5
+//!    seconds.
+//!  - A low priority task that is actually async, but will not be able to run
+//!    while the blocking task is running.
+//!  - A high priority task that prints something every second. The example
+//!    demonstrates that this task will continue to run even while the low
+//!    priority blocking task is running.
+
+// The thread-executor is created by the `#[esp_hal_embassy::main]` macro and is used to spawn `low_prio_async` and `low_prio_blocking`.
+// The interrupt-executor is created in `main` and is used to spawn `high_prio`.
+
+//% CHIPS: esp32 esp32c2 esp32c3 esp32c6 esp32h2 esp32s2 esp32s3
+//% FEATURES: embassy esp-hal-embassy/log esp-hal-embassy/integrated-timers
+
+#![no_std]
+#![no_main]
+
+use defmt::info;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Instant, Ticker, Timer};
+use esp_backtrace as _;
+use esp_hal::{
+    gpio::AnyPin,
+    interrupt::{software::SoftwareInterruptControl, Priority},
+    peripherals::RMT,
+    rmt::Rmt,
+    timer::{timg::TimerGroup, AnyTimer},
+    time::RateExtU32,
+    
+};
+use esp_hal_embassy::InterruptExecutor;
+use esp_hal_smartled::{smartLedBuffer, SmartLedsAdapter};
+use smart_leds::{
+    brightness, gamma, hsv::{hsv2rgb, Hsv}, SmartLedsWrite
+};
+use static_cell::StaticCell;
+
+#[embassy_executor::task]
+async fn high_prio(led: AnyPin, peripheral_rmt: RMT) {
+    info!("Starting high_prio()");
+    let mut ticker = Ticker::every(Duration::from_secs(1));
+    let rmt = Rmt::new(peripheral_rmt, 80.MHz()).unwrap();
+
+    let rmt_buffer = smartLedBuffer!(1);
+    let mut led = SmartLedsAdapter::new(rmt.channel0, led, rmt_buffer);
+
+    let mut color = Hsv {
+        hue: 0,
+        sat: 0,
+        val: 0,
+    };
+    let mut data;
+    loop {
+        // Iterate over the rainbow!
+        for toggle in 0..=3{
+            if toggle % 2 == 0 {
+                color.val = 0;
+                info!("OFF");
+            } else {
+                color.val = 255;
+                info!("ON");
+            }
+            data = [hsv2rgb(color)];
+            led.write(brightness(gamma(data.iter().cloned()), 150))
+                .unwrap();
+            ticker.next().await;
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn low_prio_blocking() {
+    info!("Starting low-priority task that isn't actually async");
+    loop {
+        let start = Instant::now();
+        while start.elapsed() < Duration::from_secs(5) {}
+        Timer::after(Duration::from_secs(5)).await;
+    }
+}
+
+/// A well-behaved, but starved async task.
+#[embassy_executor::task]
+async fn low_prio_async() {
+    info!("Starting low-priority task that will not be able to run while the blocking task is running");
+    let mut ticker = Ticker::every(Duration::from_secs(1));
+    loop {
+        ticker.next().await;
+    }
+}
+
+#[esp_hal_embassy::main]
+async fn main(low_prio_spawner: Spawner) {
+    info!("Init!");
+
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+
+    let sw_ints = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let timer0: AnyTimer = timg0.timer0.into();
+
+    let timg1 = TimerGroup::new(peripherals.TIMG1);
+    let timer1: AnyTimer = timg1.timer0.into();
+
+    esp_hal_embassy::init([timer0, timer1]);
+
+    static EXECUTOR: StaticCell<InterruptExecutor<2>> = StaticCell::new();
+    let executor = InterruptExecutor::new(sw_ints.software_interrupt2);
+    let executor = EXECUTOR.init(executor);
+
+    let spawner = executor.start(Priority::Priority3);
+    let rgb_48 = peripherals.GPIO48;
+    let rmt_pref = peripherals.RMT;
+
+    info!("Spawning low-priority tasks");
+    low_prio_spawner.must_spawn(low_prio_async());
+    low_prio_spawner.must_spawn(low_prio_blocking());
+
+    spawner.must_spawn(high_prio(rgb_48.into(), rmt_pref));
+
+    spawner.spawn(low_prio_async()).ok();
+
+    loop {
+        Timer::after(Duration::from_millis(20)).await;
+    }
+}
